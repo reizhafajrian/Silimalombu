5b50f32c16c9ad8640ed0fc0f1ef87d7fd87d775
reizha <reizha@importir.co>
2021-05-20 09:59:05 +0700

    text title fix

--- src/Components/SimpleComponents/TextTitile/TextTitle.js
+++ src/Components/SimpleComponents/TextTitile/TextTitle.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import styled from 'styled-components'
-import { colors } from '../../../Utils/Colors/colors'
+import React from 'react';
+import {View, Text} from 'react-native';
+import styled from 'styled-components';
+import {colors} from '../../../Utils/Colors/colors';
 
 export default function TextTitle({text}) {
-    
-    return (
-        <>
-            <TextTitleComponent color={colors.textPrimaryDark}>{text}</TextTitleComponent>
-        </>
-    )
+  return (
+    <>
+      <TextTitleComponent color={colors.textPrimaryDark}>
+        {text}
+      </TextTitleComponent>
+    </>
+  );
 }
-const TextTitleComponent=styled.Text`
-font-weight:bold;
-font-size:24px;
-color:${props=>props.color};
-width:202px
-`
+const TextTitleComponent = styled.Text`
+  font-weight: bold;
+  font-size: 23px;
+  color: ${props => props.color};
+  width: 202px;
+`;
